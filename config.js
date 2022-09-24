4ea48cf1a4eeacdee1e32cc4d513bc736d112ca8
zrScarface <114314659+zrScarface@users.noreply.github.com>
2022-09-24 23:06:39 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    app: {
+        competing: 'by Scarface ❤️',
+        global: true,
+        guild: '987801747315646534'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: true,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
---
+++ main.js
@@ -0,0 +1,30 @@
+const { Player } = require('discord-player');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+global.client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent
+    ],
+   disableMentions: 'everyone',
+});
+
+client.config = require('./config');
+
+global.player = new Player(client, client.config.opt.discordPlayer);
+
+require('./src/loader');
+require('./src/events');
+const express = require('express')
+const app = express();
+const port = 2000
+
+app.get('/', (req, res) => res.send('Odd is better.'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+client.login("MTAwNTg4MzYxNDg4NzY5MDI2MA.GK70dc.9quEbY0jGkb7hxhDNBiLnK-tY1klcFo3FiRPE0");
