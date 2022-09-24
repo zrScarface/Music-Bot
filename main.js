335daf7eaceb986704598511980ef3acc60d52e1
zrScarface <114314659+zrScarface@users.noreply.github.com>
2022-09-24 23:10:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -20,11 +20,11 @@ require('./src/loader');
 require('./src/events');
 const express = require('express')
 const app = express();
-const port = 2000
+const port = 3000
 
-app.get('/', (req, res) => res.send('Odd is better.'))
+app.get('/', (req, res) => res.send('if you are on replit'))
 
 app.listen(port, () =>
 console.log(`Your app is listening a http://localhost:${port}`)
 );
-client.login("MTAwNTg4MzYxNDg4NzY5MDI2MA.GK70dc.9quEbY0jGkb7hxhDNBiLnK-tY1klcFo3FiRPE0");
+client.login("");
